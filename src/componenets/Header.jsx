64b8ed3873089453633e0200d85aa8496951b814
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-07-19 10:51:49 +0300

    e tar val

--- src/componenets/Header.jsx
+++ src/componenets/Header.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 export function Header() {
   const [usersSearch, setUsersSearch] = useState("");
 
-  function handleChange(event) {}
-
   return (
-    <div>
-      <input onChange="text" placeholder="Enter Your Search Here..."></input>
+    <>
+      <input
+        onChange={(e) => setText(e.target.value)}
+        type="text"
+        placeholder="Enter Your Search Here..."
+      ></input>
 
       <button>Search</button>
-    </div>
+    </>
   );
 }
