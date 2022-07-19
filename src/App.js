1fdeb4a8d2baf97e8ffbcb7393745f632002e405
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-07-19 13:10:40 +0300

    state

--- src/App.js
+++ src/App.js
@@ -2,27 +2,22 @@
 import React, { useState } from "react";
 import "./App.css";
 import { Header } from "./componenets/Header";
+import { usersSearch } from "./componenets/Header";
 
 const App = () => {
   const [data, setData] = useState();
   async function getData(str) {
-    try {
-      const response = await fetch(
-        `https://www.googleapis.com/books/v1/volumes?q=${str}`
-      );
-      setData(await response.json());
-    } catch (e) {
-      console.log(e);
-    } finally {
-      console.log(`this is data from state : ${data}`);
-    }
-  }
+    const response = await fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${usersSearch}`
+    );
+    setData(await response.json());
 
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
 };
 
 export default App;
--- src/componenets/Header.jsx
+++ src/componenets/Header.jsx
@@ -1,16 +1,24 @@
 import { useState } from "react";
+
 export function Header() {
   const [usersSearch, setUsersSearch] = useState("");
 
+  let baseLink = `https://www.googleapis.com/books/v1/volumes?q=`;
+  let constructedLink = baseLink + usersSearch;
+
+  function sendSearchToServer(e) {
+    console.log(`this is sendSearchToServer running with : ${usersSearch}`);
+    console.log(`this is constructedLink : ${constructedLink}`);
+  }
   return (
     <>
       <input
         onChange={(e) => setUsersSearch(e.target.value)}
-        type="text"
-        placeholder="Enter Your Search Here..."
+        type={"text"}
+        placeHolder={"Enter Your Search Here..."}
       ></input>
 
-      <button>Search</button>
+      <button onClick={sendSearchToServer}>Search</button>
     </>
   );
 }
