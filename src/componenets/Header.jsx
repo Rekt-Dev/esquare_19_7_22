fb8adeece399aa6e51c8eb68b346ef57917ae375
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-07-19 10:38:16 +0300

    ..

--- src/componenets/Header.jsx
+++ src/componenets/Header.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 export function Header() {
   const [usersSearch, setUsersSearch] = useState("");
 
+  function handleChange(event) {}
+
   return (
     <div>
       <input placeholder="Enter Your Search Here..."></input>
